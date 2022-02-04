b43006bd15bb82bb8e1fa582e66e158ca02e2055
eoinsheehan <31827583+eoinsheehan@users.noreply.github.com>
2022-02-04 15:52:18 +0000

    Remove twitter codeblock from main.py

--- main.py
+++ main.py
@@ -32,14 +32,4 @@ total_teams = len(team_list)
 for i in range(0, total_teams, 2):
     print(team_list[i], "vs", team_list[i+1], "- On ", broadcast_list[i//2])
 
-CONSUMER_KEY = 'gaPGumV4F4R49IaUyTYnMolrw'
-CONSUMER_SECRET = 'GWw7F3YID9Q13mnSVqI8XGkwdDepL7zghz0QHLApdCCsArFYWh'
-ACCESS_KEY = '1488452318651629570-NeXZ0c9vPloP4Rwqx3DNTezXkoj5xJ'
-ACCESS_SECRET = '3jbuXd53o36WT0fU6Q3cqeaVuhpHMYaOLOaxHlfLm78U5'
-
-client = tweepy.Client(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_KEY,
-                       access_token_secret=ACCESS_SECRET)
-
-client.create_tweet(text="hello")
-
 
