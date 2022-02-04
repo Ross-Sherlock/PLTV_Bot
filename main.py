cebfbc8d47999ad1bd49c46829075ad7c4f977ea
eoinsheehan <31827583+eoinsheehan@users.noreply.github.com>
2022-02-04 14:59:52 +0000

    Revert "Add autoscroll to view full schedule"
    This reverts commit b557fda70eb7061ccc79214013fb2be4d2807bbc.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tweepy
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -14,34 +15,6 @@ driver = webdriver.Chrome(options=chrome_options, service=s)
 driver.get(url)
 
 sleep(5)
-
-## accept cookies popup
-wait = WebDriverWait(driver, 1)
-element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "js-accept-all-close")))
-element.click()
-
-## view full schedule
-wait = WebDriverWait(driver, 1)
-element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "broadcastFullSeasonButton")))
-element.click()
-
-"""A method for scrolling to the end of page"""
-
-# Get scroll height.
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-    # Scroll down to the bottom.
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # Wait to load the page.
-    sleep(2)
-    # Calculate new scroll height and compare with last scroll height.
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-
-#sleep(5)
 team_elements = driver.find_elements(By.CLASS_NAME, "teamName")
 broadcast_elements = driver.find_elements(By.CLASS_NAME, "broadcaster-text")
 team_list = []
@@ -58,4 +31,15 @@ for elem in team_elements:
 total_teams = len(team_list)
 for i in range(0, total_teams, 2):
     print(team_list[i], "vs", team_list[i+1], "- On ", broadcast_list[i//2])
+
+CONSUMER_KEY = 'gaPGumV4F4R49IaUyTYnMolrw'
+CONSUMER_SECRET = 'GWw7F3YID9Q13mnSVqI8XGkwdDepL7zghz0QHLApdCCsArFYWh'
+ACCESS_KEY = '1488452318651629570-NeXZ0c9vPloP4Rwqx3DNTezXkoj5xJ'
+ACCESS_SECRET = '3jbuXd53o36WT0fU6Q3cqeaVuhpHMYaOLOaxHlfLm78U5'
+
+client = tweepy.Client(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_KEY,
+                       access_token_secret=ACCESS_SECRET)
+
+client.create_tweet(text="hello")
+
 
