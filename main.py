397a8eb5ae045bb546c96369396fed540c632ef3
Ross-Sherlock <65044806+Ross-Sherlock@users.noreply.github.com>
2022-02-05 00:52:57 +0000

    Add date feature and change redundant code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import tweepy
+from selenium.common.exceptions import TimeoutException
+from tweet import create_tweet
+from datetime import date
+import datetime
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.headless = True
@@ -14,22 +16,57 @@ s = Service('chromedriver.exe')
 driver = webdriver.Chrome(options=chrome_options, service=s)
 driver.get(url)
 
-sleep(5)
-team_elements = driver.find_elements(By.CLASS_NAME, "teamName")
-broadcast_elements = driver.find_elements(By.CLASS_NAME, "broadcaster-text")
-team_list = []
-broadcast_list = []
+try:
+    # Wait until element located to prevent errors when finding unloaded elements
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "teams")))
+    matchList = driver.find_element(By.CLASS_NAME, "matchList")
+    team_elements = matchList.find_elements(By.CLASS_NAME, "teamName")
+    broadcast_elements = matchList.find_elements(By.CLASS_NAME, "broadcaster-text")
+    time_elements = matchList.find_elements(By.XPATH, '//time[not(@class)]')
+    date_element = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/div[2]/div[2]/time[1]/strong')
+    upcoming_date = date_element.get_attribute('innerHTML')
 
-for elem in broadcast_elements:
-    broadcast_list.append(elem.get_attribute('innerHTML'))
+    team_list = []
+    broadcast_list = []
+    time_list = []
+    date_list = []
 
-del broadcast_list[:3]
+    for elem in time_elements:
+        time_list.append(elem.get_attribute('innerHTML'))
+    for elem in broadcast_elements:
+        element = elem.get_attribute('innerHTML')
+        broadcast_list.append(element)
 
-for elem in team_elements:
-    team_list.append(elem.get_attribute('innerHTML'))
+    for elem in team_elements:
+        team_list.append(elem.get_attribute('innerHTML'))
 
-total_teams = len(team_list)
-for i in range(0, total_teams, 2):
-    print(team_list[i], "vs", team_list[i+1], "- On ", broadcast_list[i//2])
+    total_teams = len(team_list)
+    # Remove day sub-string from date string
+    date_chars = []
+    num_check = False
+    for char in upcoming_date:
+        if char.isdigit():
+            num_check = True
+        if num_check:
+            date_chars.append(char)
+    upcoming_date = "".join(date_chars, )
+
+    # Convert date string into date object
+    date_obj = datetime.datetime.strptime(upcoming_date, '%d %B %Y').date()
+    today = date.today()
+
+    # Get day difference between today and upcoming match
+    delta_days = (date_obj - today).days
+    print(delta_days)
+
+    if delta_days == 0:
+        for i in range(0, total_teams, 2):
+            tweet_str = team_list[i] + " vs " + team_list[i + 1] + " - " + broadcast_list[i // 2] + " at " + \
+                        time_list[i // 2] + " On " + upcoming_date
+            print(tweet_str)
+            create_tweet(tweet_str)
+
+except TimeoutException:
+    print("Timed out")
 
 
